b6818016ec93416fbe20820d206023f867b365fb
Thomas G Musler <thomas.g.musler@gmail.com>
2021-06-08 14:49:38 -0400

    added webpack config

---
+++ public/webpack.config.js
@@ -0,0 +1,58 @@
+const WebpackPwaManifest = require('webpack-pwa-manifest');
+const path = require('path');
+
+const config = {
+  entry: {
+    app: './public/index.js',
+  },
+  output: {
+    path: __dirname + '/dist',
+    filename: '[name].bundle.js',
+  },
+  mode: 'development',
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env'],
+          },
+        },
+      },
+    ],
+  },
+  plugins: [
+    new WebpackPwaManifest({
+      fingerprints: false,
+      name: 'Budget-Tracker',
+      short_name: 'Budget-Tracker',
+      description: 'An application that allows you to to keep track of a budget even offline',
+      background_color: '#01579b',
+      theme_color: '#ffffff',
+      'theme-color': '#ffffff',
+      start_url: '/',
+      icons: [
+        {
+          src: path.resolve('public/icons/icon-192x192.png'),
+          sizes: [96, 128, 192, 256, 384, 512],
+          destination: path.join('public', 'icons'),
+        },
+        {
+            src: path.resolve('public/icons/icon-512x512.png'),
+            sizes: [96, 128, 192, 256, 384, 512],
+            destination: path.join('public', 'icons'),
+          },
+      ],
+    }),
+  ],
+};
+
+
+
+
+
+
+module.exports = config;  
